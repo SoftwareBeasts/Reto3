5bce8e988f1aa7a8e95fd2c04e5cadeb9901f630
Aitor <aitorbringas@gmail.com>
2019-01-21 13:15:48 +0100

    Modelo terminado (de momento)

--- web/model/Pedido.php
+++ web/model/Pedido.php
@@ -68,5 +68,61 @@ class Pedido {
         $this->precioTotal = $precioTotal;
     }
 
+    public function getAll(){
+        $consulta = $this->conexion->prepare("SELECT * FROM ".$this->table);
+        $consulta->execute();
+        $resultados = $consulta->fetchAll();
 
+        $this->conexion = null;
+
+        return $resultados;
+    }
+
+    public function getByID($id){
+        $consulta = $this->conexion->prepare("SELECT * FROM ".$this->table." WHERE idpedido = :id");
+        $res = $consulta->execute(array(
+            "id" => $id
+        ));
+        $resultados = $consulta->fetch();
+
+        $this->conexion = null;
+
+        return $resultados;
+    }
+
+    public function save(){
+        $consulta = $this->conexion->prepare("INSERT INTO ".$this->table." (fecha, estado, precioTotal) VALUES (:fecha, :estado, :precioTotal)");
+        $save = $consulta->execute(array(
+            "fecha" => $this->fecha,
+            "estado" => $this->estado,
+            "precioTotal" => $this->precioTotal
+        ));
+        $this->conexion = null;
+
+        return $save;
+    }
+
+    public function deleteByID($id){
+        $consulta = $this->conexion->prepare("DELETE FROM ".$this->table." WHERE idpedido = :id");
+        $del = $consulta->execute(array(
+            "id" => $id
+        ));
+        $this->conexion = null;
+
+        return $del;
+    }
+
+    public function update(){
+        $consulta = $this->conexion->prepare("UPDATE ".$this->table." SET fecha = :fecha, estado = :estado, precioTotal = :precioTotal WHERE idpedido = :id");
+        $update = $consulta->execute(array(
+            "fecha" => $this->fecha,
+            "estado" => $this->estado,
+            "precioTotal" => $this->precioTotal,
+            "id" => $this->id
+        ));
+
+        $this->conexion = null;
+
+        return $update;
+    }
 }
