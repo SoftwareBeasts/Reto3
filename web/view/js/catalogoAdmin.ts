da26033cb1e0fb1b11711040c8f591f7c2a9b1f1
Jon Maneiro <jon.maneiro@ikasle.egibide.org>
2019-01-24 09:26:31 +0100

    Avances en catalogoAdmin

--- web/view/js/catalogoAdmin.ts
+++ web/view/js/catalogoAdmin.ts
@@ -1,10 +1,14 @@
 $('#cambiarCategoriaModal').on('show.bs.modal', function (event) {
     var button = $(event.relatedTarget) // Button that triggered the modal
-    var recipient = button.data('currname') // Extract info from data-* attributes
+    var recipient = button.data('currname')
+    var idRecipient = button.data('currid')
+    // Extract info from data-* attributes
     // If necessary, you could initiate an AJAX request here (and then do the updating in a callback).
     // Update the modal's content. We'll use jQuery here, but you could use a data binding library or other methods instead.
     var modal = $(this)
-    modal.find('.modal-body input').val(recipient)
+    modal.find('#modalFormNombreCat').val(recipient)
+    modal.find('#modalFormIdCat').val(idRecipient)
+    modal.find('#modalDeleteCatBtn').attr('href',"index.php?controller=categoria&action=deleteCategoria&id="+idRecipient)
 })
 
 $('#borrarProdModal').on('show.bs.modal', function (event) {
