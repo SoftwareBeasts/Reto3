4d9173104927db2532e89614d45b2e59aa205945
unaipuelles <unai-p@hotmail.com>
2019-01-25 13:54:41 +0100

    Cambiada configuracion email

--- web/config/emailConf.php
+++ web/config/emailConf.php
@@ -4,4 +4,6 @@ define("SEND_MAIL", false);
 define("API_KEY", "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");
 define("EMAIL_SENDER", "escueladehosteleria@softwarebeasts.ml");
 define("JS_NAME", "enviaremail.js");
-define("JS_PATH", "nodejs-compute/samples/enviaremail.js");
+define("JS_PATH", "/home/bitnami/htdocs/");
+define("JS_TO_PATH", "nodejs-compute/samples/enviaremail.js");
+define("EMAIL_MODULE_PATH", "/home/bitnami/htdocs/nodejs-compute/samples/");
