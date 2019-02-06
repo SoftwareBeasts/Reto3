7be6ca2d5db962a273d19fd9192a2f73d93596ff
Aitor <aitorbringas@gmail.com>
2019-02-06 11:58:08 +0100

    validacion carrito

--- web/view/js/cart.js
+++ web/view/js/cart.js
@@ -18,13 +18,14 @@ $(document).ready(function () {
                 $emailOK = false;
                 $telOK = false;
                 //Nombre
-                if($('#nombre').val().length < 2){$nombreOK = true;}
+                if($('#nombre').val().length >= 2){$nombreOK = true;}
                 //fecha
-                if(/((19|20)[0-9]{2})\-([0-2][0-9])\-([0-2][0-9]|[3][0-1])/.test(text)){$fechaOK = true;}
+                if(/^((19|20)[0-9]{2})\-([0-2][0-9])\-([0-2][0-9]|[3][0-1])$/.test($('#fecha').val())){$fechaOK = true;}
                 //email
-                if(/^([a-z\d!#$%&'*+\-\/=?^_`{|}~\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]+(\.[a-z\d!#$%&'*+\-\/=?^_`{|}~\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]+)*|"((([ \t]*\r\n)?[ \t]+)?([\x01-\x08\x0b\x0c\x0e-\x1f\x7f\x21\x23-\x5b\x5d-\x7e\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]|\\[\x01-\x09\x0b\x0c\x0d-\x7f\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]))*(([ \t]*\r\n)?[ \t]+)?")@(([a-z\d\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]|[a-z\d\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF][a-z\d\-._~\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]*[a-z\d\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])\.)+([a-z\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]|[a-z\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF][a-z\d\-._~\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]*[a-z\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])\.?$/.test($('#email'))){$emailOK = true;}
+                let a = new RegExp("^([a-z\\d!#$%&'*+\\-\\/=?^_`{|}~\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]+(\\.[a-z\\d!#$%&'*+\\-\\/=?^_`{|}~\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]+)*|\"((([ \\t]*\\r\\n)?[ \\t]+)?([\x01-\x08\x0b\x0c\x0e-\x1f\x7f\x21\x23-\x5b\x5d-\x7e\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]|\\\\[\x01-\x09\x0b\x0c\x0d-\x7f\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]))*(([ \\t]*\\r\\n)?[ \\t]+)?\")@(([a-z\\d\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]|[a-z\\d\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF][a-z\\d\\-._~\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]*[a-z\\d\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])\\.)+([a-z\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]|[a-z\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF][a-z\\d\\-._~\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]*[a-z\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])\\.?$");
+                if(a.test($('#email').val())){$emailOK = true;}
                 //telefono
-                if(!/^[9|6|7][0-9]{8}$/.test($('#telefono').val())){$telOK = true;}
+                if(/^[9|6|7][0-9]{8}$/.test($('#telefono').val())){$telOK = true;}
 
                 if($nombreOK && $fechaOK && $emailOK && $telOK){
                     $('#pestanaFin').toggleClass("disabled");
